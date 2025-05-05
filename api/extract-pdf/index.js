81771878f863ce3f250c9d0989a39e5c39e01f01
CalebStanish <calebdstanish@gmail.com>
2025-05-04 23:19:58 -0400

    Update index.js

--- api/extract-pdf/index.js
+++ api/extract-pdf/index.js
@@ -1,16 +1,16 @@
 export default async function (context, req) {
-  context.log("PDF tool received a request.");
+  context.log("Function hit");
 
   if (req.method !== "POST") {
     context.res = {
       status: 405,
-      body: "Method Not Allowed. Only POST is supported."
+      body: "Only POST requests allowed."
     };
     return;
   }
 
   context.res = {
     status: 200,
-    body: "Success! Your POST request was received by the Azure Function."
+    body: "Success! POST request was received."
   };
 }
